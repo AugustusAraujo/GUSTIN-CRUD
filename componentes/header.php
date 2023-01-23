4748aaf6d8d679f649794a711c53fff1aa031f8c
Augustus Araújo <71799636+AugustusAraujo@users.noreply.github.com>
2023-01-23 18:10:39 -0300

    fix: bootstrap removido do source code

--- componentes/header.php
+++ componentes/header.php
@@ -10,14 +10,10 @@ include('./componentes/db.php')
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>G-CRUD</title>
     <link rel="shortcut icon" href="./img/logo.png" type="image/x-icon">
-    <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href="./css/bootstrap.min.css">
-    <link rel="stylesheet" href="./css/bootstrap.css">
-    <link rel="stylesheet" href="./css/bootstrap-utilities.css">
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js" integrity="sha384-W8fXfP3gkOKtndU4JGtKDvXbO53Wy8SZCQHczT5FMiiqmQfUpWbYdTil/SxwZgAN" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.min.js" integrity="sha384-skAcpIdS7UcVUC05LJ9Dxay8AXcDYfBJqt1CJ85S/CFujBsIzCIv+l9liuYLaMQ/" crossorigin="anonymous"></script>
-    <script src="../js/bootstrap.bundle.js"></script>
-    
+    <?php
+    require_once "./bootstrap.php";
+    ?>
+    <link rel="stylesheet" href="style.css">   
 </head>
 <body>
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
